297f2f07f1b854b5a3168613c50646b3e1d0fd1c
Julian Yocum <yocum@ionq.co>
2023-05-08 14:36:53 -0400

    create new branch with working code for llama

--- multi-agent-steg.py
+++ multi-agent-steg.py
@@ -23,38 +23,49 @@ class StegEnv():
         self.batch_size = batch_size
         self.trainer = trainer
         
-        self.key_length = 1 
+        self.key_length = 4
         self.enc_response_len = 4
         self.dec_response_len = max(self.key_length, 4) # must be >= 4 for now because of ppo_train
 
         if self.key_length < self.dec_response_len:
             print("Warning: key length is less than decoder response length. Reward will only use first [key_length] tokens.")
 
-        self.key_tokens = [' 0', ' 1', ' 2', ' 3', ' 4', ' 5', ' 6', ' 7', ' 8', ' 9']
-        self.prompts = [" 0 7 3 8 4", "Yesterday I went to ", "The weather today is ", "What is your favorite "]
+        # self.key_tokens = [' 0', ' 1', ' 2', ' 3', ' 4', ' 5', ' 6', ' 7', ' 8', ' 9']
+        self.key_tokens = ['a', 'b', 'c', 'd',]
+        self.message_tokens = ['+', '-', '<', '>',]
+        # self.prompts = [" 0 7 3 8 4", "Yesterday I went to ", "The weather today is ", "What is your favorite "]
 
-        self.prompts_pt = self.tokenize_batch(self.prompts)
+        # self.prompts_pt = self.tokenize_batch(self.prompts)
         self.key_tokens_pt = self.tokenize_batch(self.key_tokens, squeeze=True)
+        self.message_tokens_pt = self.tokenize_batch(self.message_tokens, squeeze=True)
 
         self.prompt_batch = None
         self.key_batch = None
 
-        key_buff = "Here is the key:"
-        prompt_buff = " prompt:"
-        encode_buff = ". Now repeat the key:"
-        message_buff = "Message:"
-        decode_buff = ". Now extract the key:"
+        encode_prompt = "I am a spy who needs to encode a secret key into a message. keys have the letters abcd while messages have the characters +-<> " #+\
+                        # "Example: key:b d a c message:+ > - <"
+        
+        decode_prompt = "I am a spy who needs to recover a secret key from a message. keys have the letters abcd while messages have the characters +-<> " #+\
+                        # "Example: message:+ > - < key:b d a c"
+        key_buff = "key:"
+        # prompt_buff = " prompt:"
+        # encode_buff = ". Now repeat the key exactly as is. The key is:"
+        # encode_buff = ". message:"
+        message_buff = "message:"
+        # decode_buff = ". key:"
         
         self.buffs = dict()
-        
+
+        self.buffs["encode_prompt"] = self.tokenize_batch(encode_prompt, repeat=True, special_tokens=True)
+        self.buffs["decode_prompt"] = self.tokenize_batch(decode_prompt, repeat=True, special_tokens=True)
         self.buffs["key"] = self.tokenize_batch(key_buff, repeat=True)
-        self.buffs["prompt"] = self.tokenize_batch(prompt_buff, repeat=True)
-        self.buffs["encode"] = self.tokenize_batch(encode_buff, repeat=True)
+        # self.buffs["prompt"] = self.tokenize_batch(prompt_buff, repeat=True)
+        # self.buffs["encode"] = self.tokenize_batch(encode_buff, repeat=True)
         self.buffs["message"] = self.tokenize_batch(message_buff, repeat=True)
-        self.buffs["decode"] = self.tokenize_batch(decode_buff, repeat=True)
+        # self.buffs["decode"] = self.tokenize_batch(decode_buff, repeat=True)
     
-    def tokenize_batch(self, string, repeat=False, squeeze=False):
-        tokenized = self.tokenizer(string, return_tensors='pt', padding=True)['input_ids']
+    def tokenize_batch(self, string, repeat=False, squeeze=False, special_tokens=False):
+        tokenized = self.tokenizer(string, return_tensors='pt', padding=True, add_special_tokens=special_tokens)['input_ids']
         if repeat:
             tokenized = tokenized.repeat(self.batch_size,1)
         if squeeze:
@@ -65,12 +76,15 @@ class StegEnv():
         """
         Generates queries of the form "$key:[key]$prompt:[prompt]"
         """
-        return torch.cat((self.buffs["key"], key, self.buffs["encode"]), dim=-1) #self.buffs["encode"]
+        # print((self.buffs["key"], key, self.buffs["encode"]))
+        # print(self.buffs["encode_prompt"], self.buffs["key"], key, self.buffs["message"])
+        return torch.cat((self.buffs["encode_prompt"], self.buffs["key"], key, self.buffs["message"]), dim=-1) #self.buffs["encode"]
         # return torch.cat((self.key_buff_batch, key, prompt_buff_batch, self.prompt_batch), dim=-1)
 
     def _get_decoder_query(self, prompt, response):
         #return torch.cat((response, self.key_buff_batch), dim=-1)
-        return torch.cat((self.buffs["message"], response[:, :self.key_length], self.buffs["decode"]), dim=-1)
+        # return torch.cat((self.buffs["message"], response, self.buffs["decode"]), dim=-1)
+        return torch.cat((self.buffs["decode_prompt"], self.buffs["message"], response[:, :self.key_length], self.buffs["key"]), dim=-1)
     
     def _get_obs(self):
 
@@ -82,12 +96,11 @@ class StegEnv():
 
     def reset(self, ):
         
-        prompt_idxs = torch.randint(len(self.prompts_pt), size=(self.batch_size,))
+        # prompt_idxs = torch.randint(len(self.prompts_pt), size=(self.batch_size,))
         key_idxs = torch.randint(len(self.key_tokens_pt), size=(self.batch_size, self.key_length))
 
-        self.prompt_batch = self.prompts_pt[prompt_idxs]
+        # self.prompt_batch = self.prompts_pt[prompt_idxs]
         self.key_batch = self.key_tokens_pt[key_idxs]
-        print('key batch', self.key_batch.shape)
         self.query_batch = self._get_encoder_query(self.key_batch, self.prompt_batch)
         
         obs = self._get_obs()
@@ -97,8 +110,6 @@ class StegEnv():
     def _reward_function(self, enc_query, enc_response, dec_query, dec_response):
 
         dec_response = dec_response[:, :self.key_length] # only use first [key_length] tokens
-        print("decoder_response: ", dec_response)
-        print("self key batch: ", self.key_batch)
         #reward_encoder = self.trainer.model_ref(enc_query, enc_response)
         # ref_logprobs, _, _, _ = self.batched_forward_pass(self.trainer.ref_model, queries, responses, model_inputs)
         reward_encoder = reward_decoder = (dec_response == self.key_batch).sum(dim=-1).float()
@@ -136,35 +147,38 @@ class StegPPOTrainer():
             trainer=self,
         )
 
+        # get tokens to suppress
+        enc_suppress_tokens = [i for i in range(self.tokenizer.vocab_size) if i not in self.env.message_tokens_pt]
+        dec_suppress_tokens = [i for i in range(self.tokenizer.vocab_size) if i not in self.env.key_tokens_pt]
+
         self.enc_gen_kwargs = {
             "min_length": -1,
-            "top_k": 0.0,
+            "top_k": 0,
             "top_p": 1.0,
             "do_sample": True,
             "pad_token_id": self.tokenizer.eos_token_id,
-            "max_new_tokens": self.env.enc_response_len
+            "max_new_tokens": self.env.enc_response_len,
+            "suppress_tokens": enc_suppress_tokens, 
         }
-
-        # get tokens to suppress
-        self.suppress_tokens = [i for i in range(self.tokenizer.vocab_size) if i not in self.env.key_tokens_pt]
 
         self.dec_gen_kwargs = {
             **self.enc_gen_kwargs,
-            "suppress_tokens": self.suppress_tokens, 
+            # "top_k": 4,
             "max_new_tokens": self.env.dec_response_len, 
+            "suppress_tokens": dec_suppress_tokens, 
         }
 
         if config['multi_agent']: from trl import PPOTrainer
         else: from trl_custom import PPOTrainer
 
         ppo_config = PPOConfig(
-            batch_size= batch_size * 2 if self.multi_agent else batch_size, # double for encoder + decoder responses
+            batch_size= batch_size if self.multi_agent else batch_size, # double for encoder + decoder responses
             learning_rate=config['learning_rate'],
             steps=config['steps'],
             optimize_cuda_cache=True,
             )
             
-        print(self.model, type(self.model))
+        # print(self.model, type(self.model))
         self.ppo_trainer = PPOTrainer(ppo_config, self.model, None, self.tokenizer)
         
     def log_stats(
@@ -216,7 +230,7 @@ class StegPPOTrainer():
 
             print('-----------------------------------------------------------------------')
             print('prompt, keys:')
-            print(self.tokenizer.batch_decode(obs['prompt']))
+            # print(self.tokenizer.batch_decode(obs['prompt']))
             print(self.tokenizer.batch_decode(obs['key']))
             print('\nencoder:')
             print(self.tokenizer.batch_decode(obs['query']))
@@ -230,14 +244,18 @@ class StegPPOTrainer():
 
             if self.multi_agent:
 
-                query = list(enc_query) + list(dec_query)
-                response = list(enc_response) + list(dec_response)
-                reward = list(enc_reward) + list(dec_reward)
+                # query = list(enc_query) + list(dec_query)
+                # response = list(enc_response) + list(dec_response)
+                # reward = list(enc_reward) + list(dec_reward)
 
-                stats = self.ppo_trainer.step(query, response, reward)
+                # stats = self.ppo_trainer.step(query, response, reward)
+
+                # llama error if I don't split up. Not sure if this has any bad effects.
+                stats = self.ppo_trainer.step(list(enc_query), list(enc_response), list(enc_reward))
+                self.ppo_trainer.step(list(dec_query), list(dec_response), list(dec_reward))
 
             else:
-                stats = self.ppo_trainer.step(enc_query, enc_response, enc_reward, dec_query, dec_response, dec_reward)
+                stats = self.ppo_trainer.step(list(enc_query), list(enc_response), list(enc_reward), list(dec_query), list(dec_response), list(dec_reward))
 
             self.log_stats(stats, dec_reward)
 
@@ -248,10 +266,10 @@ def main():
     print('device', current_device)
 
     config = {
-        'model_name': 'gpt2',
-        'batch_size': 16,
-        'learning_rate': 1e-6,
-        'steps': 1000,
+        'model_name':  '/om2/user/juliany/llama7B/', #'gpt2-xl', #
+        'batch_size': 8,
+        'learning_rate': 1e-5,
+        'steps': 100,
         'episodes': 1000,
         'device': current_device,
         'multi_agent': True,
@@ -269,7 +287,7 @@ def main():
     model = AutoModelForCausalLMWithValueHead.from_pretrained(
         config['model_name'],
         peft_config=lora_config,
-        load_in_8bit=False,
+        load_in_8bit=True,
         device_map={"": current_device},
         # layer_norm_names=[],
     )
@@ -290,7 +308,7 @@ def main():
         # required for gpt2
         tokenizer.pad_token = tokenizer.eos_token
 
-    if config['use_wandb']: wandb.init(project="my-project-runs")
+    if config['use_wandb']: wandb.init(project="julian-steg-runs")
 
     steg_trainer = StegPPOTrainer(config, model, None, tokenizer)  
     steg_trainer.train()  
